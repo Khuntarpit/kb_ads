5bc8ade4e895c337ce2f0c3042197aae6b5bdeea
Khunt Arpit <66566247+Khuntarpit@users.noreply.github.com>
2022-05-29 22:26:42 +0530

    remove get

--- lib/elements/kb_ads_helper.dart
+++ lib/elements/kb_ads_helper.dart
@@ -1,8 +1,4 @@
-
 import 'dart:math';
-
-import 'package:get/get.dart';
-
 class KBAdsHelper {
 
   static String get10DigitNumber(){
@@ -40,53 +36,24 @@ class KBAdsHelper {
   }
 
   static String get bannerAdUnitId {
-    if (GetPlatform.isAndroid) {
       return _bannerId ?? 'ca-app-pub-3940256099942544/6300978111';
-    } else if (GetPlatform.isIOS) {
-      return _bannerId ?? 'ca-app-pub-3940256099942544/2934735716';
-    } else {
-      throw UnsupportedError('Unsupported platform');
-    }
   }
 
   static String get interstitialAdUnitId {
-    if (GetPlatform.isAndroid) {
       return _interstitialId ?? "ca-app-pub-3940256099942544/1033173712";
-    } else if (GetPlatform.isIOS) {
-      return _interstitialId ?? "ca-app-pub-3940256099942544/4411468910";
-    } else {
-      throw UnsupportedError("Unsupported platform");
-    }
   }
 
 
   static String get nativeAdUnitId {
-    if (GetPlatform.isAndroid) {
       return _nativeAdId ?? 'ca-app-pub-3940256099942544/2247696110';
-    } else if (GetPlatform.isIOS) {
-      return _nativeAdId ?? 'ca-app-pub-3940256099942544/3986624511';
-    }
-    throw UnsupportedError("Unsupported platform");
   }
 
 
   static String get rewardedAdUnitId {
-    if (GetPlatform.isAndroid) {
       return _rewardedAdId ?? "ca-app-pub-3940256099942544/5224354917";
-    } else if (GetPlatform.isIOS) {
-      return _rewardedAdId ?? "ca-app-pub-3940256099942544/1712485313";
-    } else {
-      throw UnsupportedError("Unsupported platform");
-    }
   }
 
   static String get appOpenAdUnitId {
-    if (GetPlatform.isAndroid) {
       return _appOpenId ?? "ca-app-pub-3940256099942544/3419835294";
-    } else if (GetPlatform.isIOS) {
-      return _appOpenId ?? "ca-app-pub-3940256099942544/5662855259";
-    } else {
-      throw UnsupportedError("Unsupported platform");
-    }
   }
 }
